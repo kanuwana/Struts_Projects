ee5590fd019d32b44a34fc1dbfdad3b19de6b452
kanuwana <skanuwana@gmail.com>
2013-12-22 18:35:06 -0500

    First Commit for hellostruts

---
+++ hellostruts/src/com/kanuwana/hellostruts/Hello.java
@@ -0,0 +1,18 @@
+package com.kanuwana.hellostruts;
+
+public class Hello {
+	
+	public String execute()
+	{
+		System.out.println("execute()");
+		return "success";
+		
+	}
+	
+	public String start()
+	{
+		System.out.println("Start()");
+		return "success";
+	}
+
+}
